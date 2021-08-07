7662dfb9b3b282aa101f8d13bc75f24f71697b66
Hamid Soleymani <58057153+hsoleymani@users.noreply.github.com>
2021-08-07 11:05:43 -0400

    Update rand_walk_all_directions.py

--- rand_walk_all_directions.py
+++ rand_walk_all_directions.py
@@ -17,8 +17,7 @@ nn = st.sidebar.number_input('Insert a number of run',min_value=10, step=100)
 x = numpy.zeros(n)
 y = numpy.zeros(n)
 dst = []
-
-
+median_dst = []
 # looping over large numver of trials
 for m in range(1,nn):
 
@@ -29,35 +28,41 @@ for m in range(1,nn):
         randy = random.choice([-1,1])
                       
         x[i] = x[i - 1] + randx
-        y[i] = y[i - 1] + randy*(numpy.sqrt(1-randx**2))
+        y[i] = y[i - 1] + randy*(1-randx**2)
         
         
     # calculate the distance in every run and store the distances
     dst.append(distance.euclidean([0,0], [x[n-1],y[n-1]]))
-            
-
+    median_dst.append(numpy.median(dst))
 # plotting section:
 
 fig = plt.pyplot.figure()
 
-plt.pyplot.subplots_adjust(hspace = .3)
+plt.pyplot.subplots_adjust(hspace = .6)
 
-ax1 = fig.add_subplot(2,1,1)
+ax1 = fig.add_subplot(3,1,1)
 ax1.plot(x, y, alpha=0.8)
 ax1.set_xlabel('Distance')
 ax1.set_ylabel('Distance')
-ax1.set_title("Random Walk ($n = " + str(n-1) + "$ steps)")
+ax1.set_title("Instance route ($n = " + str(n-1) + "$ steps)")
 ax1.scatter(x, y)
 ax1.scatter(x[0], y[0], c='r', label="Start")
 ax1.scatter(x[n-1],y[n-1], c='k', label="End")
 ax1.legend(loc='upper left')
-ax1.axis('equal')
 
 
-
-ax2 = fig.add_subplot(2,1,2)
+ax2 = fig.add_subplot(3,1,2)
 ax2.hist(dst, density=True, histtype='bar', ec='black')
 ax2.set_xlabel('Distance')
 ax2.set_ylabel('Prob Density')
+
+
+
+ax3 = fig.add_subplot(3,1,3)
+ax3.plot(median_dst)
+ax3.set_xlabel('number of runs')
+ax3.set_ylabel('median dist')
+
+
 
 st.write(fig)
